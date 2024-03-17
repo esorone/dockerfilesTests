ee3f15a2f7fcbac9bf3f49d832122472599ff9d5
esorone <esorone@gmail.com>
2024-03-17 16:16:53 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,32 @@
-#!/bin/bash
-if [ $# -eq 0 ]
-  then
-    tag='latest'
-  else
-    tag=$1
+#!/usr/bin/env bash
+set -e
+
+if ! grep ${SSH_USER} /etc/passwd ; then
+    # Install dependencies
+    apt-get install -y ${SSH_DEPENDENCIES}
+
+	# Create the user with ssh access
+	useradd -d "/home/${SSH_USER}" -m -s "/bin/bash" "${SSH_USER}"
+	mkdir -p /home/${SSH_USER}/.ssh/
+	chmod 700 /home/${SSH_USER}/.ssh
+
+	# Configure ssh_config
+	mkdir -p /var/run/sshd
+	echo "PasswordAuthentication no" >> /etc/ssh/sshd_config
+	echo "PermitRootLogin no" >> /etc/ssh/sshd_config
+	echo "AllowUsers ${SSH_USER}" >> /etc/ssh/sshd_config
+
+	# Copy the authorized_keys
+	touch /home/${SSH_USER}/.ssh/authorized_keys
+	IFS=',' read -a keys <<< "${SSH_KEY}"
+    	for key in "${keys[@]}"
+    	do
+		echo "$key" >> /home/${SSH_USER}/.ssh/authorized_keys
+    	done
+	chmod 600 /home/${SSH_USER}/.ssh/authorized_keys
+	
+	# Change the owner of the ssh folder
+	chown -R ${SSH_USER}:${SSH_USER} /home/${SSH_USER}/.ssh
 fi
 
-docker build -t project:$tag .
+exec "$@"
